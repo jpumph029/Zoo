71d43c5eb3c908c43283792b53910704e15bb69c
Joseph <pumphrey.joseph@gmail.com>
2019-01-15 13:43:50 -0800

    SeaLion is actually done

--- Zoo/Classes/SeaLion.cs
+++ Zoo/Classes/SeaLion.cs
@@ -4,19 +4,8 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class SeaLion : Aquatic
+    abstract class SeaLion : Aquatic
     {
         public override int Legs { get; set; } = 4;
-        public override bool Play()
-        {
-            Console.Write("I am not playing.\n");
-            return false;
-        }
-
-        public override bool Speak()
-        {
-            Console.Write("BARK BARK\n");
-            return true;
-        }
     }
 }
