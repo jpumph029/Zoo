2e880313f6332a61dbb4f953b2f4deef096896ee
Joseph <pumphrey.joseph@gmail.com>
2019-01-15 13:44:33 -0800

    Fixed SeaLion AGAIN

--- Zoo/Classes/SeaLion.cs
+++ Zoo/Classes/SeaLion.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    abstract class SeaLion : Aquatic
+    class SeaLion : Aquatic
     {
         public override int Legs { get; set; } = 4;
+        public override bool Play()
+        {
+            Console.Write("I am not playing.\n");
+            return false;
+        }
+
+        public override bool Speak()
+        {
+            Console.Write("BARK BARK\n");
+            return true;
+        }
     }
 }
