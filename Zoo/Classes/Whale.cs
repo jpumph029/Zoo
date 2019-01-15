d4d16b5f67317140d12d8fb48b5c8d23adca27d7
Joseph <pumphrey.joseph@gmail.com>
2019-01-15 13:46:38 -0800

    Whale class done

--- Zoo/Classes/Whale.cs
+++ Zoo/Classes/Whale.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Zoo.Classes
 {
-    class Whale
+    class Whale : Aquatic
     {
+        public override int Legs { get; set; } = 0;
+
+        public override bool Play()
+        {
+            Console.Write("I am not playing.\n");
+            return false;
+        }
+
+        public override bool Speak()
+        {
+            Console.Write("*Whale noises*\n");
+            return true;
+        }
     }
 }
